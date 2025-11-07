8b62f26d1852df30bd9579800a31c1fc0276b10a
GreyPaldin <diman.fedorenko@gmail.com>
2025-11-07 19:47:33 +0700

    ЛБ 1 нормально

--- LB1_Base/Core/Inc/init.h
+++ LB1_Base/Core/Inc/init.h
@@ -21,50 +21,50 @@
 #define GPIOC_ODR        (*(volatile uint32_t*)(0x40011000U + 0x0CU))
 
 // ==================== БИТОВЫЕ МАСКИ ДЛЯ PA2 (LED1)====================
-#define RCC_GPIOA_EN     0x00000004U  // Bit 2
-#define RCC_GPIOC_EN     0x00000010U  // Bit 4
+#define RCC_GPIOA_EN     0x00000004U  
+#define RCC_GPIOC_EN     0x00000010U  
 
-#define GPIOA_MODE_PA2_OUT       0x00000300U  // CRL[11:8] = 0011
+#define GPIOA_MODE_PA2_OUT       0x00000300U  
 #define GPIOA_MODE_PA2_MASK      ~(0x00000F00U)
 
-#define GPIOA_BSRR_PA2_SET       0x00000004U  // BSRR bit 2
-#define GPIOA_BSRR_PA2_RESET     0x00040000U  // BSRR bit 18
+#define GPIOA_BSRR_PA2_SET       0x00000004U  
+#define GPIOA_BSRR_PA2_RESET     0x00040000U  
 
 // ==================== БИТОВЫЕ МАСКИ ДЛЯ PA5 (LED2) ====================
-#define GPIOA_MODE_PA5_OUT       0x00300000U  // CRL[23:20] = 0011
+#define GPIOA_MODE_PA5_OUT       0x00300000U 
 #define GPIOA_MODE_PA5_MASK      ~(0x00F00000U)
 
-#define GPIOA_BSRR_PA5_SET       0x00000020U  // BSRR bit 5
-#define GPIOA_BSRR_PA5_RESET     0x00200000U  // BSRR bit 21
+#define GPIOA_BSRR_PA5_SET       0x00000020U  
+#define GPIOA_BSRR_PA5_RESET     0x00200000U  
 
 // ==================== БИТОВЫЕ МАСКИ ДЛЯ PA6 (LED3) ====================
-#define GPIOA_MODE_PA6_OUT       0x03000000U  // CRL[27:24] = 0011
+#define GPIOA_MODE_PA6_OUT       0x03000000U  
 #define GPIOA_MODE_PA6_MASK      ~(0x0F000000U)
 
-#define GPIOA_BSRR_PA6_SET       0x00000040U  // BSRR bit 6
-#define GPIOA_BSRR_PA6_RESET     0x00400000U  // BSRR bit 22
+#define GPIOA_BSRR_PA6_SET       0x00000040U  
+#define GPIOA_BSRR_PA6_RESET     0x00400000U   
 
 // ==================== БИТОВЫЕ МАСКИ ДЛЯ PA3 (LED4) ====================
-#define GPIOA_MODE_PA3_OUT       0x00003000U  // CRL[15:12] = 0011
+#define GPIOA_MODE_PA3_OUT       0x00003000U  
 #define GPIOA_MODE_PA3_MASK      ~(0x0000F000U)
 
-#define GPIOA_BSRR_PA3_SET       0x00000008U  // BSRR bit 3
-#define GPIOA_BSRR_PA3_RESET     0x00080000U  // BSRR bit 19
+#define GPIOA_BSRR_PA3_SET       0x00000008U  
+#define GPIOA_BSRR_PA3_RESET     0x00080000U  
 
 // ==================== БИТОВЫЕ МАСКИ ДЛЯ PA0 (Кнопка 1) ====================
-#define GPIOA_MODE_PA0_INPUT     0x00000008U  // CRL[3:0] = 1000
-#define GPIOA_MODE_PA0_OUTPUT    0x00000003U  // CRL[3:0] = 0011
+#define GPIOA_MODE_PA0_INPUT     0x00000008U  
+#define GPIOA_MODE_PA0_OUTPUT    0x00000003U  
 #define GPIOA_MODE_PA0_MASK      ~(0x0000000FU)
 
-#define GPIOA_PULLUP_PA0         0x00000001U  // ODR bit 0
-#define GPIOA_IDR_PA0            0x00000001U  // IDR bit 0
+#define GPIOA_PULLUP_PA0         0x00000001U  
+#define GPIOA_IDR_PA0            0x00000001U  
 
 // ==================== БИТОВЫЕ МАСКИ ДЛЯ PC14 (Кнопка 2) ====================
-#define GPIOC_MODE_PC14_INPUT    0x08000000U  // CRH[27:24] = 1000
+#define GPIOC_MODE_PC14_INPUT    0x08000000U  
 #define GPIOC_MODE_PC14_MASK     ~(0x0F000000U)
 
-#define GPIOC_PULLUP_PC14        0x00004000U  // ODR bit 14
-#define GPIOC_IDR_PC14           0x00004000U  // IDR bit 14
+#define GPIOC_PULLUP_PC14        0x00004000U  
+#define GPIOC_IDR_PC14           0x00004000U 
 
 // ==================== УТИЛИТЫ ====================
 #define READ_BIT(REG, BIT)       ((REG) & (BIT))
